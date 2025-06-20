4697f1beb787d3eb097779a9c091421feb1bb4ed
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-20 20:48:14 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
+import logo from "../assets/logo.png";
 
-
-const nav = () => {
+const Nav= () => {
   return (
     <nav  className="sticky top-0 z-50 backdrop-blur-lg border-b border-neutral-700/80">
+        <div className="container px-4 mx-auto relative text-sm">
+            <div className="flex justify-center items-center">
+                <div className="flex item-center flex-shrink-0">
+                    <img className="h-10 w-10 mr-2"
+                    src= {logo} alt="" />
+                    <span className="text-xl traking-tight">
+                        Landing
+                    </span>
+                </div>
+            </div>
+        </div>
 
     </nav>
   );
 }
 
-export default nav
+export default Nav
