281660584cffe55e3d83f2549d1324b119f15e9b
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-21 11:21:45 +0530

    Update HeroSec.jsx

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -11,13 +11,13 @@ const HeroSec = () => {
           <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl tracking-wide">
             Made with React & Tailwind CSS. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, vel!
           </p>
-          <div className="flex justify-center my-10">
-            <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
-              Start For Free
-            </a>
-            <a href="#" className="py-3 px-4 rounded-md border">
-              Documentation
-            </a>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mt-6">
+             <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 rounded-md text-white text-center w-full sm:w-auto">
+                Start For Free
+             </a>
+            <a href="#" className="py-3 px-4 rounded-md border text-center w-full sm:w-auto">
+               Documentation
+             </a>
           </div>
           <div className="flex mt-10 justfy-center">
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my4" src={video1} type ="video/mp4">Your Brouser does not support video.</video>
